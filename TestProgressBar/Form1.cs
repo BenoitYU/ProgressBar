1553799fed3ce506a3fa876901bed47d90c09672
Benoit YU <yubenoit@gmail.com>
2019-10-25 17:00:32 +0200

    添加了内部显示进度条的方法

---
+++ TestProgressBar/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace TestProgressBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.backgroundWorker1.RunWorkerAsync(); // 运行 backgroundWorker 组件
+            ProcessForm form = new ProcessForm(this.backgroundWorker1);// 显示进度条窗体
+            form.ShowDialog(this);
+            form.Close();
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+            }
+            else
+            {
+            }
+        }
+        //你可以在这个方法内，实现你的调用，方法等。
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                worker.ReportProgress(i);
+                if (worker.CancellationPending)  // 如果用户取消则跳出处理数据代码 
+                {
+                    e.Cancel = true;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ TestProgressBar/ProcessForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TestProgressBar
+{
+    partial class ProcessForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(63, 69);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(366, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ProcessForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 187);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "ProcessForm";
+            this.Text = "正在下载请稍后";
+            this.Controls.SetChildIndex(this.progressBar1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+    }
+}
